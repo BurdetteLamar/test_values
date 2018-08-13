ce00ec4b73294fedee599efabfa95505581cf4f6
burdettelamar <burdettelamar@yahoo.com>
2018-08-13 14:24:31 -0500

    More on string_values

--- lib/test_values/string_values.rb
+++ lib/test_values/string_values.rb
@@ -1,8 +1,9 @@
 class StringValues < ValuesBase
 
+  BASE_STRING = 'x'
   # Return a string of the given size.
   # Use trimmed or extended base_string, if given; otherwise 'x' characters.
-  def self.string_of_size(size, base_string='x')
+  def self.string_of_size(size, base_string=BASE_STRING)
     self.verify_class('size', Integer, size)
     self.verify_class('base_string', String, base_string)
     if size < 0
@@ -16,5 +17,16 @@ class StringValues < ValuesBase
     end
     return s[0..size-1]
   end
+
+  # Return strings of maximum and minimum length for range.
+  def self.strings_in_length_range(range, base_string=BASE_STRING)
+    self.verify_class('range', Range, range)
+    {
+        :min_length => self.string_of_size(range.first, base_string),
+        :max_length => self.string_of_size(range.last, base_string),
+    }
+  end
+
+
 
 end
--- test/string_values_test.rb
+++ test/string_values_test.rb
@@ -18,20 +18,53 @@ class StringValuesTest < Minitest::Test
       actual = StringValues.string_of_size(size, base_string)
       assert_equal(expected, actual, "size=#{size} base_string=#{base_string}")
     end
-    [
-        ['a', 'x'],
-        [1, 1],
-    ].each do |args|
+    {
+        ['a', 'x'] => /size/,
+        [1, 1] => /base_string/,
+    }.each_pair do |args, regexp|
       e = assert_raises(TypeError) do
         StringValues.string_of_size(*args)
       end
+      assert_match(regexp, e.message)
     end
-    [
-        [-1, 'x'],
-    ].each do |args|
+    {
+        [-1, 'x'] => /size/,
+    }.each_pair do |args, regexp|
       e = assert_raises(RangeError) do
         StringValues.string_of_size(*args)
       end
+      assert_match(regexp, e.message)
+    end
+  end
+
+  def test_strings_in_length_range
+    base_string = 'x'
+    expected = {}
+    [
+        (0..15),
+        (1..1),
+    ].each do |range|
+      expected.store(:min_length, base_string * range.first)
+      expected.store(:max_length, base_string * range.last)
+      actual = StringValues.strings_in_length_range(range)
+      assert_equal(expected, actual, "range=#{range}")
+    end
+    {
+        ['x', 'x'] => /range/,
+        [(0..4), 1] => /base_string/,
+    }.each_pair do |args, regexp|
+      e = assert_raises(TypeError) do
+        StringValues.strings_in_length_range(*args)
+      end
+      assert_match(regexp, e.message)
+    end
+    {
+        (-1..10) => /size/,
+    }.each_pair do |range, regexp|
+      e = assert_raises(RangeError) do
+        StringValues.strings_in_length_range(range)
+      end
+      assert_match(regexp, e.message)
     end
   end
 
